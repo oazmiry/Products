088f01f47dbeb0e954210cdd89c4ad6dc1d9dd59
Ohad Zmiry <oazmiry@gmail.com>
2020-03-22 15:53:17 +0200

    Added Item & Seller models

--- Products.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Products.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ Products.Models/DataStore/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Products.Models.DataStore
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int SellerId { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
