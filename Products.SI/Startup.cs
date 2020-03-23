e6c9925c74ba89220717824df818d26a17b0f3bb
Ohad Zmiry <oazmiry@gmail.com>
2020-03-23 13:12:48 +0200

    Refactored Startup to not look so auto-generated

--- Products.SI/Startup.cs
+++ Products.SI/Startup.cs
@@ -20,15 +20,20 @@ namespace Products.SI
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Adds services to the container.
+        /// </summary>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configures the HTTP request pipeline.
+        /// </summary>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
