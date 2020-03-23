69566a26e8cb5155cd4c426f575cd903b6f2c917
Ohad Zmiry <oazmiry@gmail.com>
2020-03-23 20:46:53 +0200

    Created the graphql controller (endpoint)

---
+++ Products.SI/Controllers/GraphqlController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using GraphQL;
+using Microsoft.AspNetCore.Mvc;
+using Products.SI.GraphQL;
+
+namespace Products.SI.Controllers
+{
+    [Route("graphql")]
+    [ApiController]
+    public class GraphqlController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] GraphQlQuery query)
+        {
+            var inputs = query.Variables.ToInputs();
+
+            var result = await new DocumentExecuter().ExecuteAsync(options =>
+            {
+                options.Schema = new GraphqlApiSchema().Schema;
+                options.Query = query.Query;
+                options.OperationName = query.OperationName;
+                options.Inputs = inputs;
+            });
+
+            if (result.Errors?.Count > 0)
+            {
+                // TODO: Maybe it won't be good in production.
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(result);
+        }
+    }
+}
