ba24e631892fde51cfc299ad2d7d74349ec06e80
Ohad Zmiry <oazmiry@gmail.com>
2020-03-23 11:35:05 +0200

    Added exceptions to separate between assemblies when propagating cross-assemblies

---
+++ Products.Exceptions/BlException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Products.Exceptions
+{
+    [Serializable]
+    public class BlException : Exception
+    {
+        public BlException()
+        {
+        }
+
+        public BlException(string message) : base(message)
+        {
+        }
+
+        public BlException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Products.Exceptions/DalException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Products.Exceptions
+{
+    [Serializable]
+    public class DalException : Exception
+    {
+        public DalException()
+        {
+        }
+
+        public DalException(string message) : base(message)
+        {
+        }
+
+        public DalException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
