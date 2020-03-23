628470f3ce41587bec166d2725d22abd712cf68d
Ohad Zmiry <oazmiry@gmail.com>
2020-03-23 20:44:41 +0200

    Added the graphql query model

---
+++ Products.SI/GraphQL/GraphQlQuery.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Products.SI.GraphQL
+{
+    /// <summary>
+    /// Structures an incoming query for graphql.
+    /// </summary>
+    public class GraphQlQuery
+    {
+        public string OperationName { get; set; }
+        public string NamedQuery { get; set; }
+        public string Query { get; set; }
+        public JObject Variables { get; set; }
+    }
+}
