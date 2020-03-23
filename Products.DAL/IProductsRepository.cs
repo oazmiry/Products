a205eea337457f5a35a4629112dc18cac4a0e80d
Ohad Zmiry <oazmiry@gmail.com>
2020-03-23 13:25:54 +0200

    Added a repository for this app

---
+++ Products.DAL/IProductsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Products.Models.DataStore;
+
+namespace Products.DAL
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Seller> FetchAllSellers();
+        Seller GetSellerOrDefault(int id);
+        Item GetItemOrDefault(int id);
+        IEnumerable<Item> FetchAllItems();
+    }
+}
---
+++ Products.DAL/ProductsRepository.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Products.Exceptions;
+using Products.Models.DataStore;
+
+namespace Products.DAL
+{
+    public class ProductsRepository: IProductsRepository
+    {
+        private readonly IStoreContextFactory _contextFactory;
+        private readonly string _connectionString;
+
+        public ProductsRepository(IStoreContextFactory contextFactory, IConfiguration config)
+        {
+            _contextFactory = contextFactory;
+            _connectionString = config.GetConnectionString("Store");
+        }
+
+        public IEnumerable<Seller> FetchAllSellers()
+        {
+            try
+            {
+                using (var context = _contextFactory.GetContext(_connectionString))
+                {
+                    return context.Sellers.ToList();
+                }
+            }
+            catch (SqlException e)
+            {
+                // TODO: Log
+                throw new DalException(e.Message, e);
+            }
+        }
+
+        public IEnumerable<Item> FetchAllItems()
+        {
+            try
+            {
+                using (var context = _contextFactory.GetContext(_connectionString))
+                {
+                    return context.Items.ToList();
+                }
+            }
+            catch (SqlException e)
+            {
+                // TODO: Log
+                throw new DalException(e.Message, e);
+            }
+        }
+
+        public Seller GetSellerOrDefault(int id)
+        {
+            try
+            {
+                using (var context = _contextFactory.GetContext(_connectionString))
+                {
+                    return context.Sellers.Find(id);
+                }
+            }
+            catch (SqlException e)
+            {
+                // TODO: Log
+                throw new DalException(e.Message, e);
+            }
+        }
+
+        public Item GetItemOrDefault(int id)
+        {
+            try
+            {
+                using (var context = _contextFactory.GetContext(_connectionString))
+                {
+                    return context.Items.Find(id);
+                }
+            }
+            catch (SqlException e)
+            {
+                // TODO: Log
+                throw new DalException(e.Message, e);
+            }
+        }
+    }
+}
