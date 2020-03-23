76a3ec00ea6a3f1e0c78b542dac8597e712c8659
Ohad Zmiry <oazmiry@gmail.com>
2020-03-23 23:40:52 +0200

    Removed useless NamedQuery property from GraphQlQuery struct
    After searching the web to find if it's necessary

--- Products.SI/GraphQL/GraphQlQuery.cs
+++ Products.SI/GraphQL/GraphQlQuery.cs
@@ -8,7 +8,6 @@ namespace Products.SI.GraphQL
     public class GraphQlQuery
     {
         public string OperationName { get; set; }
-        public string NamedQuery { get; set; }
         public string Query { get; set; }
         public JObject Variables { get; set; }
     }
