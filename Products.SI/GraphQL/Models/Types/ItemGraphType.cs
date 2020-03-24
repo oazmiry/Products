c7354ea924d32d9974656d0fd8d578ba85b02d02
Ohad Zmiry <oazmiry@gmail.com>
2020-03-24 16:09:21 +0200

    Created objects for GraphTypes for item & seller

---
+++ Products.SI/GraphQL/Models/Types/ItemGraphType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using Products.Models.DataStore;
+
+namespace Products.SI.GraphQL.Models.Types
+{
+    /// <summary>
+    /// States which fields are exposed by the api for the <see cref="Item"/> type.
+    /// </summary>
+    public class ItemGraphType : ObjectGraphType<Item>
+    {
+        // TODO: Maybe I'll need to add the types classes to the service container.
+        public ItemGraphType()
+        {
+            Field(item => item.Id).Description("Item's id");
+            Field(item => item.Description).Description("Describes the item");
+            Field(item => item.SellerId).Description("Item's owner id");
+            Field(item => item.Seller).Description("Item's seller object");
+        }
+    }
+}
---
+++ Products.SI/GraphQL/Models/Types/SellerGraphType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using Products.Models.DataStore;
+
+namespace Products.SI.GraphQL.Models.Types
+{
+    /// <summary>
+    /// States which fields are exposed by the api for the <see cref="Seller"/> type.
+    /// </summary>
+    public class SellerGraphType : ObjectGraphType<Seller>
+    {
+        public SellerGraphType()
+        {
+            Field(seller => seller.Id).Description("Seller's id");
+            Field(seller => seller.Name).Description("Seller's full name");
+            Field(seller => seller.Items).Description("All items the seller owns");
+        }
+    }
+}
