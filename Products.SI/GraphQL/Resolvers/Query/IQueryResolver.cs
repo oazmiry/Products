d9dde1b351af9c65897bc159e3498965b6b81c94
Ohad Zmiry <oazmiry@gmail.com>
2020-03-24 16:14:40 +0200

    Moved query resolver to its own namespace, added interface, subclassing ObjectGraphType

---
+++ Products.SI/GraphQL/Resolvers/Query/IQueryResolver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using GraphQL.Types;
+using Products.Models.DataStore;
+
+namespace Products.SI.GraphQL.Resolvers.Query
+{
+    /// <summary>
+    /// Should be implemented in order to declare that the class holds fields which handle a query. 
+    /// </summary>
+    public interface IQueryResolver : IObjectGraphType
+    {
+    }
+}
---
+++ Products.SI/GraphQL/Resolvers/Query/QueryResolver.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using GraphQL;
+using GraphQL.Types;
+using Products.BL;
+using Products.Models.DataStore;
+using Products.SI.GraphQL.Models.Types;
+
+namespace Products.SI.GraphQL.Resolvers.Query
+{
+    /// <summary>
+    /// Resolves a business logic action for a GraphQL query.
+    /// This is necessary to decouple the business logic layer from GraphQL,
+    /// which is service-interface-layer-related.
+    /// </summary>
+    [GraphQLMetadata("Query")]
+    public class QueryResolver : ObjectGraphType, IQueryResolver
+    {
+        public QueryResolver(IProductsBusinessLogic bl)
+        {
+            Field<ListGraphType<SellerGraphType>>(
+                "sellers",
+                resolve: _ => bl.GetAllSellers()
+                );
+            Field<SellerGraphType>(
+                "seller",
+                arguments: new QueryArguments(
+                    new QueryArgument<IdGraphType> { Name = "id" }
+                ),
+                resolve: context => bl.GetSeller(context.GetArgument<int>("id"))
+                );
+            Field<ListGraphType<ItemGraphType>>(
+                "items",
+                resolve: _ => bl.GetAllItems()
+                );
+            Field<ItemGraphType>(
+                "item",
+                arguments: new QueryArguments(
+                    new QueryArgument<IdGraphType> { Name = "id" }
+                ),
+                resolve: context => bl.GetItem(context.GetArgument<int>("id"))
+                );
+        }
+    }
+}
